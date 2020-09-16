c8b2661668cbacb4f107682fde66a71fea1b7a24
In Code We Trust <njderenne@gmail.com>
2020-09-15 23:21:34 -0500

    completed one round of battle with alive/died alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,15 +5,27 @@ var playerAttack = 10;
 
 var enemyName = "Roberto"
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 102;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
     enemyHealth = enemyHealth - playerAttack;
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
     playerHealth = playerHealth - enemyAttack;
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
