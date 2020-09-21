007b673adc4326c1c50492e5c868d5996b49340d
In Code We Trust <njderenne@gmail.com>
2020-09-20 21:56:25 -0500

    added highscore feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -101,6 +101,19 @@ var shop = function() {
 
 
 var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat teh high score of " + highScore + ". Maybe next time!");
+    }
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
     } else {
